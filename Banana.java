ff95795e79abed8818167cf809690d9e64d32b2f
Brianna-Chan <348835448@gapps.yrdsb.ca>
2024-09-12 13:02:17 -0400

    reset banana

--- Banana.java
+++ Banana.java
@@ -16,5 +16,23 @@ public class Banana extends Actor
     {
         // Add your action code here.
         move(-10);
+        
+        if(getX() <= 0)
+        {
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0)
+        {
+            setLocation(600,100);
+        }
+        else
+        {
+            setLocation(600,300);
+        }
     }
 }
