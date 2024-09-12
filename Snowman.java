c5e52a3918313e6dd6aa4d47d0d6b2ff44f69f35
Brianna-Chan <348835448@gapps.yrdsb.ca>
2024-09-12 12:39:24 -0400

    add snowman

---
+++ Snowman.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Snowman here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Snowman extends Actor
+{
+    boolean atTop = true;
+    /**
+     * Act - do whatever the Snowman wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if(Greenfoot.mouseClicked(null))
+        {
+            atTop = !atTop;
+        }
+        
+        if(atTop)
+        {
+            setLocation(100,100);
+        }
+        else
+        {
+            setLocation(100,300);
+        }
+    }
+}
